8efa07d31d192a09a2e88341aac8d818a22363fc
Parth Shah <helloparthshah@gmail.com>
2023-01-31 18:05:54 -0800

    added logo to navbar

--- src/App.js
+++ src/App.js
@@ -1,38 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Header from './Header';
 
 function App() {
   return (
-    <Navbar bg="light" expand="lg">
+    <>
+      <Header />
       <Container>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" /></header>
-        <Navbar.Brand href="#home">Atharav Solutions</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </Navbar.Collapse>
+        <h1>Home Page</h1>
       </Container>
-    </Navbar>
+    </>
   );
 }
 
